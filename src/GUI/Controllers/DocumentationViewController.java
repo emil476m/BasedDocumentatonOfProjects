4e02008dcf112f342c337129f796190d62dae7a8
emil476m <emilwiltmann.dk@gmail.com>
2023-05-01 14:44:24 +0200

    fix for exception when opening DocumentationViewController

--- src/GUI/Controllers/DocumentationViewController.java
+++ src/GUI/Controllers/DocumentationViewController.java
@@ -392,7 +392,8 @@ public class DocumentationViewController extends BaseController{
 
     private void setUpTechniciansRemoveAddAndView(){
         try {
-            getModelsHandler().getCeoModel().getUserOnCurrentProject().addAll(getModelsHandler().getCeoModel().getUsersWorkingOnProject(opnedProject));
+            if (opnedProject != null)
+                getModelsHandler().getCeoModel().getUserOnCurrentProject().addAll(getModelsHandler().getCeoModel().getUsersWorkingOnProject(opnedProject));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
